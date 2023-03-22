9b4ef948f4e6ab1546e62e3a2d2cc48668ff3ce6
Oscar Esteban Pacheco Cabrera <124205704+ALbaz145@users.noreply.github.com>
2023-03-22 19:41:38 +0000

    ringo bongo

---
+++ Musicos/Program.cs
@@ -0,0 +1,39 @@
+﻿
+internal class Program
+{
+    class Musico
+    {
+    public string nombre{get; set;}
+    public Musico
+    {
+
+    }
+    public void Saluda() 
+    {
+        Console.WriteLine("Hola soy {0}",nombre);
+
+    }
+    public virtual void Toca()
+    {
+        Console.WriteLine($"{Nombre} esta tocando su intrumento");
+    }
+
+}
+class Bajista:Musico
+{
+    public string bajo {get; set;}
+    public Bajista(string n, string b):base(n)
+    {
+        bajo = b;
+    }
+    public override void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su {bajo}")
+    } 
+}
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
